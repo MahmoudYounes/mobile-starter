696fb8150423ea08a48b3e603e0f4a8bf604cc93
mahmoud <m.younesbadr@hotmail.com>
2018-12-31 07:30:26 +0200

    adding more UI elements to restaurant.js

--- App/components/Restaurants/Restaurant.js
+++ App/components/Restaurants/Restaurant.js
@@ -2,16 +2,78 @@ import React from 'react';
 import {
 	View,
 	Text,
-	Image,
+	ImageBackground,
+	TouchableHighlight,
 	StyleSheet
 } from 'react-native';
+import {
+	Icon, Button
+} from 'react-native-elements';
 
+var restaurantStyles = StyleSheet.create({
+	cardView:{
+		borderRadius: 21,
+		borderWidth: 1,
+		height:500,
+		backgroundColor: "white"
+	},
+	cardTitle: {
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
+		backgroundColor:"#56A8FB",		
+		padding:10, 
+		flex: 1,
+		flexDirection: "row"
+	},
+	cardTitleText: {
+		color: "white",
+		fontSize: 20,
+	},
+	backgroundImageStyle: {
+		width:"100%", 
+		height:"80%",
+		marginTop: 60,
+	},
+	RestaurantTitle: {
+		flex: 1, 
+		flexDirection: "row", 
+		alignItems: "flex-start",
+		padding: 10,
+		backgroundColor: 'rgba(0, 0, 0, .3)',
+	},
+	restaurantTitleText: {
+		color: "white",
+		fontSize: 20,
+		paddingLeft:10,
+		fontFamily:"helvetica",
+	}
+});
 
 const Restaurant = (props) => {
 	var restaurant = props.restaurant;
+	var imageUri = restaurant.images && restaurant.images.length > 0 ? restaurant.images[0] : "";
+	var restaurantType = restaurant.description && restaurant.description.slice(restaurant.description.indexOf(',') + 1).trim() || "Generic cusine";
 	return (
-		<View style={{alignItems: 'center', padding: 20}} key="1">
-			<Text style={{color: "black"}}>{restaurant.title}</Text>
+		<View style={restaurantStyles.cardView} key="1">
+			<View style={restaurantStyles.cardTitle}>
+				<Text style={restaurantStyles.cardTitleText}>{restaurantType}</Text>
+				<TouchableHighlight 
+					style={{
+						marginLeft:"auto",
+						borderRadius: 32,
+						backgroundColor: 'white'
+					}}>
+					<Icon name="pin" type="octicon" color="#56A8FB" />
+				</TouchableHighlight>
+			</View>
+			<ImageBackground source={{uri:imageUri}} style={restaurantStyles.backgroundImageStyle}>
+				<View style={restaurantStyles.RestaurantTitle}>
+					<Icon name="location-pin" color="#fff" type="simple-line-icon" />
+					<Text style={restaurantStyles.restaurantTitleText}>
+					{restaurant.title}
+					</Text>
+				</View>
+			</ImageBackground>
 		</View>
 	);
 }
--- App/components/Restaurants/Restaurants.js
+++ App/components/Restaurants/Restaurants.js
@@ -13,6 +13,18 @@ import { RESTAURANT_SEARCH_QUERY } from '../../graphql/queries';
 //import RestaurantsList  from './RestaurantsList';
 import Restaurant from './Restaurant';
 
+
+const mainStyles = StyleSheet.create({
+  container:{
+    width: "100%",
+    height: "100%",
+    padding: 10,
+    paddingBottom: 60,
+    paddingTop: 70,
+    backgroundColor: "#eff4ff"
+  }
+});
+
 export default class Restaurants extends React.Component {
   state = {
   };
@@ -20,9 +32,9 @@ export default class Restaurants extends React.Component {
   render() {
     // TODO: This shouldn't be hard coded.  Allow the user to enter it into a Text Input
     const address = '1260 6th Ave, New York, NY 10020';
-
+    
     return (
-      <View style={{marginTop: 50}}>
+      <View style={mainStyles.container}>
         <Query
           query={RESTAURANT_SEARCH_QUERY}
           variables={{
@@ -41,20 +53,17 @@ export default class Restaurants extends React.Component {
 
             var mainStyles = StyleSheet.create({
               container:{
-                flex:1
               }
             });
             if (data.search_restaurants && data.search_restaurants.results && data.search_restaurants.results.length > 0) {
-              var restaurants = data.search_restaurants.results.filter((elem) => elem.images && elem.images.length > 0);
+              var restaurants = data.search_restaurants.results.filter((elem) => elem.images && elem.images.length > 0)
+              .map((elem, idx) => {
+                return <Restaurant restaurant={elem} key={idx}></Restaurant>
+              });
               return (
-                  <View>
-                    <View>
-                      <Text>Hard 1</Text>
-                    </View>
-                    <View>
-                      <Text>Hard 2</Text>
-                    </View>
-                  </View>
+                  <ViewPagerAndroid style={{flex: 2}}>
+                    {restaurants}
+                  </ViewPagerAndroid>
               );
             }
 
