0d8cd9e390a1dab61105da00dcb17491f3bb50b5
mahmoud <m.younesbadr@hotmail.com>
2019-01-14 23:45:51 +0200

    finished card page layout

--- App/components/Restaurants/Restaurants.js
+++ App/components/Restaurants/Restaurants.js
@@ -24,7 +24,7 @@ export default class Restaurants extends React.Component {
     // TODO: This shouldn't be hard coded.  Allow the user to enter it into a Text Input
     const address = '1260 6th Ave, New York, NY 10020';
     return (
-      <View style={{flex: 1}}>
+      <View style={mainStyles.container}>
         <Query
           query={RESTAURANT_SEARCH_QUERY}
           variables={{
@@ -43,36 +43,40 @@ export default class Restaurants extends React.Component {
 
             if (data.search_restaurants && data.search_restaurants.results && data.search_restaurants.results.length > 0) {
               return (
-                  <ViewPagerAndroid style={mainStyles.container}>
+                  <ViewPagerAndroid style={{flex: 1}}>
                     {
                       data.search_restaurants.results.filter((elem) => elem.images && elem.images.length > 0)
                       .map((restaurant, idx) => {
                         var imageUri = restaurant.images && restaurant.images.length > 0 ? restaurant.images[0] : "";
-                        var restaurantType = restaurant.description && restaurant.description.slice(restaurant.description.indexOf(',') + 1).trim() || "Generic cusine";
+                        var restaurantType = restaurant.description && restaurant.description.slice(restaurant.description.indexOf(',') + 1).trim() || "American Standard Cusine";
                         return (
-                          <View style={{flex:1}} key={idx}>
-                            <View style={restaurantStyles.cardHeader}>
-                              <Text style={restaurantStyles.cardTitleText}>{restaurantType}</Text>
-                              <TouchableHighlight style={restaurantStyles.pinRestaurant}>
-                              <Icon name="pin" type="octicon" color="#56A8FB" />
-                              </TouchableHighlight>
+                          <View style={mainStyles.restaurantPage} key={idx}>
+                            <View style={restaurantStyles.cardView}>
+                              <View style={restaurantStyles.cardHeader}>
+                                <Text style={restaurantStyles.cardTitleText}>{restaurantType}</Text>
+                                <TouchableHighlight style={restaurantStyles.pinRestaurant}>
+                                <Icon name="pin" type="octicon" color="#56A8FB" />
+                                </TouchableHighlight>
+                              </View>
+                              <View style={restaurantStyles.cardInfo}>
+                                  <Text>how long</Text>
+                              </View>
+                              <View style={restaurantStyles.cardBody}>
+                                  <ImageBackground source={{uri:imageUri}} style={restaurantStyles.backgroundImage}>
+                                  <View style={restaurantStyles.RestaurantTitle}>
+                                      <Icon name="location-pin" color="#fff" type="simple-line-icon" />
+                                      <Text style={restaurantStyles.restaurantTitleText}>
+                                      {restaurant.title}
+                                      </Text>
+                                  </View>
+                                  </ImageBackground>
+                              </View>
+                              <View style={restaurantStyles.cardFooter}>
+                                  <Text>Call</Text>
+                                  <Text>How Far Away</Text>
+                                  <Text>Save</Text>
+                              </View>                           
                             </View>
-                            <View style={restaurantStyles.cardInfo}>
-                                <Text>how long</Text>
-                            </View>
-                            <View style={restaurantStyles.cardBody}>
-                                <ImageBackground source={{uri:imageUri}} style={restaurantStyles.backgroundImage}>
-                                <View style={restaurantStyles.RestaurantTitle}>
-                                    <Icon name="location-pin" color="#fff" type="simple-line-icon" />
-                                    <Text style={restaurantStyles.restaurantTitleText}>
-                                    {restaurant.title}
-                                    </Text>
-                                </View>
-                                </ImageBackground>
-                            </View>
-                            <View style={restaurantStyles.cardFooter}>
-                                <Text>this is the footer</Text>
-                            </View>                            
                           </View>
                         )
                       })
@@ -96,24 +100,34 @@ export default class Restaurants extends React.Component {
 const mainStyles = StyleSheet.create({
   container:{
     flex: 1,
+    padding: 30,
+    paddingTop: 40,
+    backgroundColor: "#cce6ff"
+  },
+  restaurantPage: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderRadius: 21,
+		borderWidth: 0,
+    flex: 1,
   }
 });
+
 var restaurantStyles = StyleSheet.create({
 	cardView:{
-    borderRadius: 21,
-		borderWidth: 1,
-		flex: 1,
+    flex: 1,
 	},
 	cardHeader: {
     flex: 1,
-		//flexDirection: "row",
 		borderTopLeftRadius: 20,
 		borderTopRightRadius: 20,
 		backgroundColor: "#4285f4"
   },
   cardTitleText: {
 		color: "white",
-		fontSize: 20,
+    fontSize: 18,
+    alignSelf: "stretch",
+    padding: 5
   },
   pinRestaurant: {
 		borderRadius: 32,
@@ -123,8 +137,8 @@ var restaurantStyles = StyleSheet.create({
 		right: 15,
 	},
 	cardInfo: {
-		backgroundColor: "white",
-		flex: 1,
+    flex: 1,
+    padding: 5
 	},
 	cardBody: {
 		flex: 7,
@@ -133,7 +147,10 @@ var restaurantStyles = StyleSheet.create({
 		flex: 1
   },
   RestaurantTitle: {
-		backgroundColor: 'rgba(0, 0, 0, .3)',
+    backgroundColor: 'rgba(0, 0, 0, .3)',
+    flexDirection: "row",
+    alignContent: "space-between",
+    padding: 10
   },
   restaurantTitleText: {
 		color: "white",
@@ -141,10 +158,11 @@ var restaurantStyles = StyleSheet.create({
 		fontFamily:"helvetica",
 	},
 	cardFooter: {
-		backgroundColor: "black",
 		borderBottomLeftRadius: 20,
 		borderBottomRightRadius: 20,
-		flex:1,
-		backgroundColor: "black"
+    flex:1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 5
 	}
 });
