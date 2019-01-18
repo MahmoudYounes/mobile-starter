39943efb934309a731ec480f2de4c7536eccd743
mahmoud <m.younesbadr@hotmail.com>
2019-01-18 17:14:09 +0200

    finished main requirements

---
+++ App/components/Restaurants/RestaurantViewPager.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ViewPagerAndroid,
+    ImageBackground,
+    TouchableHighlight,
+    Linking
+} from 'react-native';
+import {
+    Icon,
+} from 'react-native-elements';
+
+
+export default class RestaurantViewPager extends React.Component {
+
+    callRestaurant(phoneNumber) {
+        if (phoneNumber && !isNaN(phoneNumber)) {
+            let link = `tel:${phoneNumber}`;
+            console.log(link);
+            Linking.canOpenURL(link).then(supported => {
+                if (!supported) {
+                    console.log("this url structure is not supported");
+                }
+                else {
+                    return Linking.openURL(link);
+                }
+            }).catch(err => {
+                console.error(err);
+            })
+        }
+    }
+
+    render() {
+        return (
+            <ViewPagerAndroid peekEnabled={true} pageMargin={20} style={{ flex: 1 }}>
+                {
+                    this.props.restaurants
+                        .map((restaurant, idx) => {
+                            var imageUri = restaurant.images && restaurant.images.length > 0 ? restaurant.images[0] : "";
+                            var restaurantType = restaurant.description && restaurant.description.slice(restaurant.description.indexOf(',') + 1).trim() || "American Standard Cusine";
+                            return (
+                                <View style={restaurantStyles.restaurantPage} key={idx}>
+                                    <View style={restaurantStyles.cardView}>
+                                        <View style={restaurantStyles.cardHeader}>
+                                            <Text style={restaurantStyles.cardTitleText}>{restaurantType}</Text>
+                                            <TouchableHighlight style={restaurantStyles.pinRestaurant}>
+                                                <Icon name="pin" type="octicon" color="#56A8FB" />
+                                            </TouchableHighlight>
+                                        </View>
+                                        <View style={restaurantStyles.cardInfo}>
+                                            <View style={restaurantStyles.cardInfoItem}>
+                                                <Icon name="stars" type="materialicon" color="#4285f4" />
+                                                <Text style={{ color: "#001f4d", paddingTop: 1 }}>{restaurant.rating}</Text>
+                                            </View>
+                                            <View style={restaurantStyles.cardInfoItem}>
+                                                <Icon name="drive-eta" type="materialicon" color="#4285f4" />
+                                                <Text style={{ color: "#001f4d", paddingTop: 1 }}>{parseInt(restaurant.distance) + 1} km</Text>
+                                            </View>
+                                            <View style={restaurantStyles.cardInfoItem}>
+                                                <Icon name="drive-eta" type="materialicon" color="#4285f4" />
+                                                <Text style={{ color: "#001f4d", paddingTop: 1 }}>{parseInt(restaurant.distance) + 1} km</Text>
+                                            </View>
+                                        </View>
+                                        <View style={restaurantStyles.cardBody}>
+                                            <ImageBackground source={{ uri: imageUri }} style={restaurantStyles.backgroundImage}>
+                                                <View style={restaurantStyles.RestaurantTitle}>
+                                                    <Icon name="location-pin" color="#fff" type="simple-line-icon" />
+                                                    <Text style={restaurantStyles.restaurantTitleText}>
+                                                        {decodeURIComponent(restaurant.title)}
+                                                    </Text>
+                                                </View>
+                                            </ImageBackground>
+                                        </View>
+                                        <View style={restaurantStyles.cardFooter}>
+                                            <TouchableHighlight underlayColor="white" onPress={() => this.callRestaurant(restaurant.phone)}>
+                                                <Text style={restaurantStyles.buttonText}>Call</Text>
+                                            </TouchableHighlight>
+                                            <TouchableHighlight>
+                                                <Text style={restaurantStyles.buttonText}>Directions</Text>
+                                            </TouchableHighlight>
+                                            <TouchableHighlight>
+                                                <Text style={restaurantStyles.buttonText}>Share</Text>
+                                            </TouchableHighlight>
+                                        </View>
+                                    </View>
+                                </View>
+                            )
+                        })
+                }
+            </ViewPagerAndroid >
+        );
+    }
+}
+
+var restaurantStyles = StyleSheet.create({
+    restaurantPage: {
+        flex: 1,
+        backgroundColor: "#fff",
+        borderRadius: 21,
+        borderWidth: 0,
+        flex: 1,
+    },
+    cardView: {
+        flex: 1,
+    },
+    cardHeader: {
+        flex: 1,
+        flexDirection: "row",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        backgroundColor: "#4285f4",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingRight: 10,
+        paddingLeft: 5
+    },
+    cardTitleText: {
+        color: "white",
+        fontSize: 18,
+        flex: 0.75
+    },
+    pinRestaurant: {
+        borderRadius: 32,
+        backgroundColor: 'white',
+    },
+    cardInfo: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingRight: 5,
+        paddingLeft: 5
+    },
+    cardInfoItem: {
+        flexDirection: "row",
+    },
+    cardBody: {
+        flex: 7,
+    },
+    backgroundImage: {
+        flex: 1
+    },
+    RestaurantTitle: {
+        backgroundColor: 'rgba(0, 0, 0, .4)',
+        flexDirection: "row",
+        alignContent: "space-between",
+        padding: 10
+    },
+    restaurantTitleText: {
+        color: "white",
+        fontSize: 20,
+        fontFamily: "helvetica",
+    },
+    cardFooter: {
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 5
+    },
+    buttonText: {
+        color: "#001f4d"
+    }
+});
+
+
--- App/components/Restaurants/Restaurants.js
+++ App/components/Restaurants/Restaurants.js
@@ -4,17 +4,12 @@ import {
   ActivityIndicator,
   Text,
   StyleSheet,
-  ViewPagerAndroid,
-  ImageBackground,
-	TouchableHighlight,
 } from 'react-native';
-import {
-	Icon,
-} from 'react-native-elements';
 
 import { Query } from 'react-apollo';
 import { RESTAURANT_SEARCH_QUERY } from '../../graphql/queries';
-//import Restaurant from './Restaurant';
+
+import RestaurantViewPager from './RestaurantViewPager';
 
 export default class Restaurants extends React.Component {
   state = {
@@ -35,61 +30,16 @@ export default class Restaurants extends React.Component {
             if (loading) {
               return (
                 <View style={{ width: '100%', paddingVertical: 10, alignContent: "center" }}>
-                  <Text style={{alignSelf:"center"}}>Loading...</Text>
-                  <ActivityIndicator size="large" style={{ padding: 30}} />
+                  <Text style={{ alignSelf: "center" }}>Loading...</Text>
+                  <ActivityIndicator size="large" style={{ padding: 30 }} />
                 </View>
               );
             }
 
             if (data.search_restaurants && data.search_restaurants.results && data.search_restaurants.results.length > 0) {
+              var restaurants = data.search_restaurants.results.filter((elem) => elem.images && elem.images.length > 0);
               return (
-                  <ViewPagerAndroid style={{flex: 1}}>
-                    {
-                      data.search_restaurants.results.filter((elem) => elem.images && elem.images.length > 0)
-                      .map((restaurant, idx) => {
-                        var imageUri = restaurant.images && restaurant.images.length > 0 ? restaurant.images[0] : "";
-                        var restaurantType = restaurant.description && restaurant.description.slice(restaurant.description.indexOf(',') + 1).trim() || "American Standard Cusine";
-                        console.log(restaurant);
-                        return (
-                          <View style={mainStyles.restaurantPage} key={idx}>
-                            <View style={restaurantStyles.cardView}>
-                              <View style={restaurantStyles.cardHeader}>
-                                <Text style={restaurantStyles.cardTitleText}>{restaurantType}</Text>
-                                <TouchableHighlight style={restaurantStyles.pinRestaurant}>
-                                <Icon name="pin" type="octicon" color="#56A8FB" />
-                                </TouchableHighlight>
-                              </View>
-                              <View style={restaurantStyles.cardInfo}>
-                                  <View style={restaurantStyles.cardInfoItem}>
-                                    <Icon name="stars" type="materialicon" color="#4285f4"/>
-                                    <Text style={{color: "#001f4d", paddingTop:1}}>{restaurant.rating}</Text>
-                                  </View>
-                                  <View style={restaurantStyles.cardInfoItem}>
-                                    <Icon name="drive-eta" type="materialicon" color="#4285f4"/>
-                                    <Text style={{color: "#001f4d", paddingTop:1}}>{parseInt(restaurant.distance) + 1} km</Text>
-                                  </View>
-                              </View>
-                              <View style={restaurantStyles.cardBody}>
-                                  <ImageBackground source={{uri:imageUri}} style={restaurantStyles.backgroundImage}>
-                                  <View style={restaurantStyles.RestaurantTitle}>
-                                      <Icon name="location-pin" color="#fff" type="simple-line-icon" />
-                                      <Text style={restaurantStyles.restaurantTitleText}>
-                                      {restaurant.title}
-                                      </Text>
-                                  </View>
-                                  </ImageBackground>
-                              </View>
-                              <View style={restaurantStyles.cardFooter}>
-                                  <Text>Call</Text>
-                                  <Text>How Far Away</Text>
-                                  <Text>Save</Text>
-                              </View>                           
-                            </View>
-                          </View>
-                        )
-                      })
-                    }
-                  </ViewPagerAndroid> 
+                <RestaurantViewPager restaurants={restaurants}></RestaurantViewPager>
               );
             }
             // No Data Return
@@ -106,78 +56,11 @@ export default class Restaurants extends React.Component {
 }
 
 const mainStyles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    padding: 30,
+    padding: 10,
+    paddingBottom: 40,
     paddingTop: 40,
     backgroundColor: "#cce6ff"
-  },
-  restaurantPage: {
-    flex: 1,
-    backgroundColor: "#fff",
-    borderRadius: 21,
-		borderWidth: 0,
-    flex: 1,
   }
-});
-
-var restaurantStyles = StyleSheet.create({
-	cardView:{
-    flex: 1,
-	},
-	cardHeader: {
-    flex: 1,
-		borderTopLeftRadius: 20,
-		borderTopRightRadius: 20,
-		backgroundColor: "#4285f4"
-  },
-  cardTitleText: {
-		color: "white",
-    fontSize: 18,
-    alignSelf: "stretch",
-    padding: 5
-  },
-  pinRestaurant: {
-		borderRadius: 32,
-		backgroundColor: 'white',
-		position: "absolute",
-		top:15,
-		right: 15,
-	},
-	cardInfo: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignContent: "space-around",
-  },
-  cardInfoItem: {
-    flex: 1,
-    flexDirection: "row",
-    alignSelf:"center",
-  },
-	cardBody: {
-		flex: 7,
-  },
-  backgroundImage: {
-		flex: 1
-  },
-  RestaurantTitle: {
-    backgroundColor: 'rgba(0, 0, 0, .3)',
-    flexDirection: "row",
-    alignContent: "space-between",
-    padding: 10
-  },
-  restaurantTitleText: {
-		color: "white",
-		fontSize: 20,
-		fontFamily:"helvetica",
-	},
-	cardFooter: {
-		borderBottomLeftRadius: 20,
-		borderBottomRightRadius: 20,
-    flex:1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 5
-	}
 });
